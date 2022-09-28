1d40cb5fc84fce03d767da27ff7894ad26a51571
George <georgea.fotheringham@gmail.com>
2022-09-28 14:11:43 -0400

    links array shuffle and counter while fixing re-render issue

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,6 +1,21 @@
 import  React, { useState }  from 'react';
 import './App.css';
 import ImageLoader from './component/imageLoader';
+import aphrodite from './images/aphrodite.png';
+import ares from  './images/ares.png';
+import artemis from  './images/artemis.png';
+import daphne from  './images/daphne.png';
+import echo from  './images/echo.png';
+import eris from  './images/eris.png';
+import eros from  './images/eros.png';
+import hades from  './images/hades.png';
+import hecates from  './images/hecate.png';
+import hephaestus from './images/hephaestus.png';
+import hera from   './images/hera.png';
+import hermes from  './images/hermes.png';
+import persephone from  './images/persephone.png';
+import poseidon from  './images/poseidon.png';
+import psyche from  './images/psyche.png'; 
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -8,7 +23,22 @@ const App = () => {
   const incrementCount = () => {
     setCount(count + 1);
   };
-  
+
+    const [imageSourceArray, setArray] = useState([aphrodite, ares, artemis, daphne, echo, eris, eros, hades, hecates, hephaestus, hera, hermes, persephone, poseidon, psyche]);
+
+    const getRandom = () => {
+        let currentIndex = imageSourceArray.length, randomIndex;
+      
+        console.log('runs')
+        while (currentIndex !== 0) {
+            randomIndex= Math.floor(Math.random()*currentIndex);
+            currentIndex--; 
+            [imageSourceArray[currentIndex], imageSourceArray[randomIndex]] = [imageSourceArray[randomIndex], imageSourceArray[currentIndex]]             
+        };
+        setArray(imageSourceArray);      
+        return incrementCount()  
+    };
+    console.log('app runs')
   return (
     <div>
       <div>
@@ -17,10 +47,9 @@ const App = () => {
       </div>
       <div>
         <div>{count}</div>
-        <button onClick={incrementCount}>Increment Count</button>
         </div>  
         <div>
-          <ImageLoader/>
+          <ImageLoader imageSourceArray={imageSourceArray} getRandom={getRandom}/>
         </div>
     </div>
   );
--- memory-card/src/component/imageLoader.js
+++ memory-card/src/component/imageLoader.js
@@ -1,33 +1,7 @@
 import React, { useState } from "react";
-import aphrodite from '../images/aphrodite.png';
-import ares from  '../images/ares.png';
-import artemis from  '../images/artemis.png';
-import daphne from  '../images/daphne.png';
-import echo from  '../images/echo.png';
-import eris from  '../images/eris.png';
-import eros from  '../images/eros.png';
-import hades from  '../images/hades.png';
-import hecates from  '../images/hecate.png';
-import hephaestus from '../images/hephaestus.png';
-import hera from   '../images/hera.png';
-import hermes from  '../images/hermes.png';
-import persephone from  '../images/persephone.png';
-import poseidon from  '../images/poseidon.png';
-import psyche from  '../images/psyche.png'; 
-const LoadImages = () => {
-    const [imageSourceArray, setArray] = useState([aphrodite, ares, artemis, daphne, echo, eris, eros, hades, hecates, hephaestus, hera, hermes, persephone, poseidon, psyche]);
 
-    const getRandom = () => {
-        let currentIndex = imageSourceArray.length, randomIndex;
-        
-        console.log('runs')
-        while (currentIndex !== 0) {
-            randomIndex= Math.floor(Math.random()*currentIndex);
-            currentIndex--; 
-            [imageSourceArray[currentIndex], imageSourceArray[randomIndex]] = [imageSourceArray[randomIndex], imageSourceArray[currentIndex]]             
-        };
-        return setArray(imageSourceArray);        
-    };
+const LoadImages = (props) => {
+    const {imageSourceArray, getRandom} = props;
 
     return (
         <div>
