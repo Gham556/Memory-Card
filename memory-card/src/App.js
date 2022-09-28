21e1cbcf1d846212132e5c676bcbf1f07f7422d4
George <georgea.fotheringham@gmail.com>
2022-09-28 13:58:58 -0400

    succesfully loads images and shuffles array but doesn't re-render on click

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,6 +1,6 @@
-
 import  React, { useState }  from 'react';
 import './App.css';
+import ImageLoader from './component/imageLoader';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -11,8 +11,17 @@ const App = () => {
   
   return (
     <div>
-      <div>{count}</div>
-      <button onClick={incrementCount}>Increment Count</button>
+      <div>
+        <h1>Memory Game!</h1>
+        <h2>Click An Image To Increase Your Score, But Be Sure Not To Click The Same Image Twice!</h2>
+      </div>
+      <div>
+        <div>{count}</div>
+        <button onClick={incrementCount}>Increment Count</button>
+        </div>  
+        <div>
+          <ImageLoader/>
+        </div>
     </div>
   );
 }
